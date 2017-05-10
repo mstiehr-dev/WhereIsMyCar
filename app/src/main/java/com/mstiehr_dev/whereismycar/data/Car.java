3848286b8dca020720ae9afb193e74c2b55b04b8
Martin <git@maltin.pisces.uberspace.de>
2017-05-10 20:02:07 +0200

    add POJOs

--- app/src/main/java/com/mstiehr_dev/whereismycar/data/Car.java
+++ app/src/main/java/com/mstiehr_dev/whereismycar/data/Car.java
@@ -2,5 +2,49 @@ package com.mstiehr_dev.whereismycar.data;
 
 public class Car
 {
-    // TODO: 08.05.17 copy from github-client app
+    private long id;
+    private String description;
+    private double latitude;
+    private double longitude;
+
+    public Car() {
+    }
+
+    public Car(long id, double latitude, double longitude) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- app/src/main/java/com/mstiehr_dev/whereismycar/data/Driver.java
+++ app/src/main/java/com/mstiehr_dev/whereismycar/data/Driver.java
@@ -2,5 +2,50 @@ package com.mstiehr_dev.whereismycar.data;
 
 public class Driver
 {
-    // TODO: 08.05.17 copy from github-client app 
+    private long id;
+    private String username;
+    private String email;
+    private String password; // probably hash of an passphrase
+
+    public Driver() {
+    }
+
+    public Driver(long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
